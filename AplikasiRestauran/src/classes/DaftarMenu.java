604b3e8c2758077a3f6d5002be7ee5ef08bbcf3d
ilhamzaenudin <rosniati2909@gmail.com>
2021-02-17 08:30:02 +0700

    Implementasi Struktur Kelas Aplikasi Restaurant

---
+++ AplikasiRestauran/src/classes/DaftarMenu.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package classes;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author user
+ */
+public class DaftarMenu {
+    private ArrayList<Menu> daftarMenu;
+    
+    public DaftarMenu() {}
+    
+    public void tambahMenu (Menu menu) {}
+    
+    public void getMenuByKategori(String kategori) {}
+    
+    public void tampilDaftarMenu() {}
+}
---
+++ AplikasiRestauran/src/classes/Pesanan.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package classes;
+
+/**
+ *
+ * @author user
+ */
+public class Pesanan {
+    private Menu menu;
+    private int jumlah;
+    private String keterangan;
+    
+    public Pesanan(Menu menu, int jumlah) {}
+    
+    public void setKeterangan(String keterangan) {}
+    
+    public String getKeterangan() { return ""; }
+}
